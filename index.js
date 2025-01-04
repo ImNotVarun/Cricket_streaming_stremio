1b8f4ecc6b2c67de3a3f7d12fd47718cd5d0e91a
Varun <imnotvarun@gmail.com>
2025-01-04 19:41:56 +0530

    updated

--- index.js
+++
@@ -1,4 +0,0 @@
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
-const addonInterface = require("./src/addon");
-
-serveHTTP(addonInterface, { port: process.env.PORT || 3000 });
